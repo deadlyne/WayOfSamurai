e438bab881f122d98df3152aa7eed967213a032a
Max Domnitsky <kocher555@gmail.com>
2021-01-25 19:51:11 +0300

    Add grid and pictures and menu

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://cdni.iconscout.com/illustration/free/thumb/mountain-biking-2080785-1763858.png' />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+          </div>
+        <div>
+          <a>Messagies</a>
+          </div>
+          <div>
+            <a>News</a>
+          </div>
+          <div>
+            <a>Music</a>
+          </div>
+          <div>
+            <a>Settings</a>
+          </div>
+      </nav>
+      <div className='content'>
+        <div>
+        <img src='https://pixnio.com/free-images/2018/12/08/2018-12-08-16-25-38.jpg' />
+        </div>
+        <div>
+          Ava + descripsion
+        </div>
+        <div>My posts
+          <div>
+            New post
+          </div>
+          <div>
+            Post 1
+          </div>
+          <div>
+            Post 2
+          </div>
+        </div>
+    </div>
     </div>);
 }
-
-const Technologies = () => {
-  return (
-    <div>
-      <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
-    </div>);
-}
-
-const Header = () => {
-  return (
-    <div>
-      <a href='#s'>Home</a>
-      <a href='#s'>News Feed</a>
-      <a href='#s'>Messagies</a>
-    </div>);
-}
-
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (<div>
+      <a href='#s'>Home</a> -
+      <a href='#s'>News Feed</a> -
+      <a href='#s'>Messagies</a> -
+    </div>);
+}
+
+  export default Header;
